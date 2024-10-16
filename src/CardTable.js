a178c8b5657f634bc9d74b3b072dcbed4b9f04ce
Johanna Eriksson <je224gs@student.lnu.se>
2024-10-16 11:14:25 +0200

    Get rid of unneeded discardPile field in cardtable

--- src/CardTable.js
+++ src/CardTable.js
@@ -2,22 +2,19 @@ export class CardTable {
   #players
   #cardsInPlay
   #cardDeck
-  #discardPile
 
   constructor() {
     this.#players = []
     this.#cardsInPlay = []
     this.#cardDeck = new CardDeck('kille')
-    this.#discardPile = []
   }
 
   playRound(startingPlayer = null) {
-    // shuffle deck.
-    // deal cards to players, 5 each.
-    // for each player, call playCard method
-    // once each player has played a card, check who played the highest card
-    // set the player who played the highest card as the starting player for the next round
-    // move played cards to the discard pile
+    this.#shuffleDeck()
+    this.#dealCards()
+    // determine starting player
+    this.#playCards()
+    this.#endRound()
   }
 
   #shuffleDeck() {
@@ -32,6 +29,7 @@ export class CardTable {
     }
   }
 
+  // Make objects in array, to keep track of who played what card?
   #playCards() {
     this.#players.forEach(player => {
       this.#cardsInPlay.push(player.playCard(this.#getHighestCard()))
@@ -40,5 +38,20 @@ export class CardTable {
 
   #getHighestCard() {
     return this.#cardsInPlay.sort((a, b) => a.rank - b.rank)[0]
+  }
+
+  #endRound() {
+    const startingPlayer = this.#determineWinner()
+    this.#addCardsBackToDeck()
+    this.playRound(startingPlayer)
+  }
+
+  #determineWinner() {
+    // check who played the highest card
+    // return the winner
+  }
+
+  #addCardsBackToDeck() {
+    this.#cardsInPlay.forEach(card => this.#cardDeck.add(card))
   }
 }
